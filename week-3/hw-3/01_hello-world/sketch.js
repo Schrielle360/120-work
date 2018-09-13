a2ad50b0df349520946816fc1a9d0910ac83218c
Schrielle360 <Schrielle360@users.noreply.github.com>
2018-09-12 20:43:02 -0600

    Did first part of homework

--- week-3/hw-3/01_hello-world/sketch.js
+++ week-3/hw-3/01_hello-world/sketch.js
@@ -1,3 +1,19 @@
-function setup(){
-  ellipse(20, 20, 30, 30);
+// setup; In p5, the setup function is where we place commands that relate to “setting up” a code sketch. p5 calls and executes the setup() function once, at the start of executing every p5 sketch.
+function setup() {
+
+    // create a canvas 600px wide and 600px high
+    createCanvas( 600, 600 );
+
+    // color the background 'purple'
+    background( 'purple' );
+
+
+
+    
+}// This is where you would put writing or other things.
+function draw() {
+
+    // tell p5 to print the text string "Hello World!"
+    // This is going to be positioned at ( x: 20, y: 30 ).
+    text( "Hello World!", 20, 30 );
 }
