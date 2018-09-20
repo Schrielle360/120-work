b6ddce1fec5ab45e762d7a7e9e8d1449b5a97c60
Schrielle360 <Schrielle360@users.noreply.github.com>
2018-09-20 00:42:03 -0600

    slowly making changes

--- hw-4/sketch.js
+++ hw-4/sketch.js
@@ -1,22 +1,29 @@
 function setup() {
   //creating a canvas to draw on
-  createCanvas( 900, 600)
+  createCanvas(600, 900)
 }
 
 function draw() {
   // the background color
-  background( 'rgb(99, 66, 10)')
+  background( 'rgb(99, 66, 10)');
+
 }
 // created a sandbox for the entire thing
 push();
-// moving the grid
+// moving the grid (x:0, y:0)
+// to canvas center
 
-translate( 450, 300);
+translate( 300, 450);
 
 // BODY
 
 push();
-//body code here
+rect( -150, -100, 300, 300);
+
+//background
+fill( 'rgba( 0, 0, 0, 1.0)');
+
+
 
 pop();
 // END OF BODY
@@ -35,13 +42,24 @@ pop();
 //HEAD
 push();
 
-//head code here
-//MAKE BASE FIRST
+
+    stroke( 0 );
+    fill('rgba(255, 119, 0, 1.0)');
+    ellipse( 0, 0, 400 );
+
+
 
 //THEN MOUTH
 
 push();
-//mouth code here
+// ** draw the smiley face mouth **
+// This is centered on the 0-point,
+// of the x-axis
+noStroke();
+fill( 40 );
+arc( 0, 50, 300, 200, 0, PI );
+
+
 
 pop();
 //END OF mouth
@@ -49,8 +67,16 @@ pop();
 
 // EYES
 push();
-//EYES HERE
 
+// In other words they have the
+// same y-value (-15px)
+// And the x-value is mirrored
+// (left:-20px right:20px)
+fill( 40 );
+// ** left eye **
+ellipse( -100, -75, 100 );
+// ** right eye **
+ellipse( 100, -75, 100 );
 pop();
 //end of eyes
 
