525e71367ebb576d42002e3a33bab84d8a1350c3
Schrielle360 <Schrielle360@users.noreply.github.com>
2018-10-03 19:52:55 -0600

    changed shit

--- hw-6/halloween/empty-example/sketch.js
+++ hw-6/halloween/empty-example/sketch.js
@@ -1,13 +1,34 @@
 // defining pumpkin variables
 let windowWidth = 800;
-let x = 0
+let x = -400
 let y = 0
-let candy = {};
-candy.pos1x = mouseX;
-candy.pos1y = mouseY;
-candy.color = fill('red');
-candy.size = stroke(30);
-let 
+let pumpkin = {
+  fill('orange');
+  ellipse(x, y, 200, 100); //base of the head
+
+  fill('black');
+  //eyes
+  triangle(x-50, y-35, x-50, y-10, x-15, y-10); // left eye
+  triangle(x+50, y-35, x+50, y-10, x+15, y-10); //right eye
+  triangle(x, y-10, x-10, y+10, x+10, y+10); // nose
+
+  //mouth
+
+  triangle(x-50, y+10, x-50, y+30, x-25, y+20); //first triangle left of mouth
+  triangle(x-30, y+10, x-30, y+30, x+5, y+20); //second triangle left of mouth
+  triangle(x-5, y+20, x+30, y+10, x+30, y+30); //first triangle right of mouth
+  triangle(x+20, y+20, x+50, y+10, x+50, y+30); // second triangle right of mouth
+
+  // the stem
+  fill('green'); //making stem green
+  rect(x-10, y-70, 15, 20);
+
+  fill('yellow'); //eye pupils
+  ellipse(x-30, y-15, 5, 5); //left pupil
+  ellipse(x+30, y-15, 5, 5); //right pupil
+
+}
+
 // we can then assign qualities to the object
 
 
@@ -27,7 +48,36 @@ function draw() {
   text('Happy Halloween!', 25, 100);
 
 
+// translate the canvas to center being 0
+translate (400, 400); //0 is now the center of the canvas
 
+//now making the head
+
+push();
+fill('orange');
+ellipse(x, y, 200, 100); //base of the head
+
+fill('black');
+//eyes
+triangle(x-50, y-35, x-50, y-10, x-15, y-10); // left eye
+triangle(x+50, y-35, x+50, y-10, x+15, y-10); //right eye
+triangle(x, y-10, x-10, y+10, x+10, y+10); // nose
+
+//mouth
+
+triangle(x-50, y+10, x-50, y+30, x-25, y+20); //first triangle left of mouth
+triangle(x-30, y+10, x-30, y+30, x+5, y+20); //second triangle left of mouth
+triangle(x-5, y+20, x+30, y+10, x+30, y+30); //first triangle right of mouth
+triangle(x+20, y+20, x+50, y+10, x+50, y+30); // second triangle right of mouth
+
+// the stem
+fill('green'); //making stem green
+rect(x-10, y-70, 15, 20);
+
+fill('yellow'); //eye pupils
+ellipse(x-30, y-15, 5, 5); //left pupil
+ellipse(x+30, y-15, 5, 5); //right pupil
+pop();
 
 //this makes the pumpkin move across the screen
 x=x+1
