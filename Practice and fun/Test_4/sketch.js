47ddfb0617cc64158737edca1899c6061788c1f1
Schrielle360 <Schrielle360@users.noreply.github.com>
2018-12-06 22:34:32 -0700

    updating the sketchh part of the program to add more chibis

--- Practice and fun/Test_4/sketch.js
+++ Practice and fun/Test_4/sketch.js
@@ -1,5 +1,6 @@
 let chibi;
 var img;
+const numOfChibis = 20;
 
 function preload(){
   img = loadImage('catgirl.png');
@@ -10,15 +11,28 @@ function setup() {
   createCanvas( windowWidth, 600);
 
 //creates a new chibi of the object of class 'chibi'
-  chibi = new Chibi (width/2, height/2, 80 );
+  let init_x = 60;
+  let init_y = 60;
+  for (Let i = 0; i < numOfChibiss; i++){
+    chibis.push( new Chibi(init_x, init_y) );
+    //move the starting position over
+    // chibis wont start on eachother
+    init_x += 100;
+    if(init_x > width) {
+      init_x = 60;
+      init_y += 100;
+    }
+  }
 }
 
 
 function draw() {
   background('indigo');
 
+  for (let i = 0; i < balls.length; i++) {
   //call the Chibi's methods
     chibi.display();
     chibi.move();
     chibi.edgeCheck();
+  }
 }
