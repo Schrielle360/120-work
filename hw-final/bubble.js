9c4bdd9cc4c4bc4ab4d9320b60ad86baee5ddb99
Schrielle360 <Schrielle360@users.noreply.github.com>
2018-12-06 23:07:14 -0700

    updated hw-12 to match test_4

--- hw-final/bubble.js
+++ hw-final/bubble.js
@@ -1,47 +1,53 @@
-Class Chibi () {
-  constructor(x, y) {
+class Chibi {
+  constructor(x, y, size) {
+    this.color = color;
+    this.size = size;
+    this.rad = this.size/ 2;
     this.x = x;
     this.y = y;
-    this.size = size;
-    this.rad = this.size / 2;
-    this.deltaX = random(-10, 10);
-    this.deltaY = random(-10, 10);
+    this.deltaX = random(-2, 2);
+    this.deltaY = random(-2, 2);
   }
-  //shows the chibi
-  display () {
-    imageMode(CENTER);
-    image(img, this.x, this.y);
-    }
-  //moves the chibi
-  move () {
-    this.x += this.deltaX;
-    this.y += this.deltaY;
-  }
-  //keeps the chibis on canvas
-  edgeCheck(){
-      // check if the ball has hit a vertical wall (left or right walls)
-    if (this.x + this.rad >= width || this.x - this.rad <= 0) {
-          this.deltaX *= -1;
-    }
-      // check if the ball has hit a horizontal wall (top or bottom walls)
-    if (this.y + this.rad >= height || this.y - this.rad <= 0) {
-          this.deltaY *= -1;
-    }
-  }
-        ChibiCheck(otherChibis, myId) {
-      // for loop touches each of the balls in the array
-          for (let i = 0; i < otherChibis.length; i++) {
-              // if i != myId, then check for touching
-              // otherwise, its ME and we need to skip
-              if (i != myId) {
-                  let d = dist(this.x, this.y, otherChibis[i].x, otherChibis[i].y);
-                  let combinedR = this.rad + otherChibis[i].rad;
 
-                  if (d <= combinedR) {
-                      this.deltaX *= -1;
-                      this.deltaY *= -1;
-                }
-            }
-        }
+  display() {
+    push();
+    //center image
+    imageMode(CENTER);
+    //the main image
+    image(img, this.x, this.y);
+    pop();
+  }
+
+  move() {
+    this.x = this.x += this.deltaX;
+    this.y = this.y += this.deltaY;
+  }
+
+  edgeCheck() {
+    // check if the Chibi has hit a vertical wall (left or right walls)
+    if( this.x + this.rad >= width || this.x - this.rad <= 0 ) {
+        this.deltaX *= -1;
     }
+    // check if the Chibi has hit a horizontal wall (top or bottom walls)
+    if( this.y + this.rad >= height || this.y - this.rad <= 0 ) {
+        this.deltaY *= -1;
+    }
+  }
+
+  chibiCheck( otherChibis, myId) {
+    //for loop touches each of the Chibis in the array
+    for (let n = 0; n < otherChibis.length; n++) {
+      //if n != myId, then check for touching
+      //otherwise, its ME and we need to skip
+      if( n != myId) {
+          let d = dist( this.x, this.y, otherChibis[n].x, otherChibis[n].y );
+          let combinedR = this.rad + otherChibis[n].rad;
+
+          if( d <= combinedR) {
+            this.deltaX *= -1;
+            this.deltaY *= -1;
+        }
+      }
+    }
+  }
 }
--- hw-final/sketch.js
+++ hw-final/sketch.js
@@ -1,38 +1,39 @@
-var chibis = [];
+let chibi = [];
 var img;
 const numOfChibis = 20;
+
 function preload(){
-    img = loadImage('catgirl.png');
+  img = loadImage('catgirl.png');
 }
 
 function setup() {
-    createCanvas(windowHeight, windowWidth);
-    // create a new chibi object of class type "chibi"
-    let init_x = 30;
-    let init_y = 30;
-    for (let i = 0; i < numOfChibis; i++) {
-        chibis.push(new Chibi(init_x, init_y));
-        // move the starting position over
-        // This is to ensure that the chibis do not start
-        // "on top" of each other
-        init_x += 100;
-        if (init_x > width) {
-            init_x = 40;
-            init_y += 70;
-        }
-    }
+  //create the canvas (windowWidth, windowHeight);
+  createCanvas( windowWidth, 600);
 
+//creates a new chibi of the object of class 'chibi'
+  let init_x = 60;
+  let init_y = 60;
+  for (Let i = 0; i < numOfChibis; i++){
+    chibis.push( new Chibi(init_x, init_y) );
+    //move the starting position over
+    // chibis wont start on eachother
+    init_x += 100;
+    if(init_x > width) {
+      init_x = 60;
+      init_y += 100;
+    }
+  }
 }
 
 
 function draw() {
-
   background('indigo');
-  for (var i = chibis.length - 1; i >= 0; i--) {
-    chibis[i].move();
-    chibis[i].display();
-    chibis[i].bubbleCheck();
-    chibis[i].edgeCheck();
 
+  for (let i = 0; i < chibis.length; i++) {
+  //call the Chibi's methods
+    chibi[i].chibiCheck(chibis, i);
+    chibi[i].display();
+    chibi[i].move();
+    chibi[i].edgeCheck();
   }
 }
