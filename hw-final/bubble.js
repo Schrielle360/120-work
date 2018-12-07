ec76dc45f387d5c4b18ff214fc16b5634de16aad
Schrielle360 <Schrielle360@users.noreply.github.com>
2018-12-07 11:43:02 -0700

    gave size a number to work with

--- hw-final/bubble.js
+++ hw-final/bubble.js
@@ -1,7 +1,7 @@
 class Chibi {
   constructor(x, y, size) {
     this.color = color;
-    this.size = size;
+    this.size = 30;
     this.rad = this.size/ 2;
     this.x = x;
     this.y = y;
@@ -32,13 +32,15 @@ class Chibi {
 
   edgeCheck() {
     // check if the Chibi has hit a vertical wall (left or right walls)
-    if( this.x + this.rad >= width || this.x - this.rad <= 0 ) {
+    if (this.x + this.rad >= width || this.x - this.rad <= 0) {
+        console.log("edge");
         this.deltaX *= -1;
     }
     // check if the Chibi has hit a horizontal wall (top or bottom walls)
-    if( this.y + this.rad >= height || this.y - this.rad <= 0 ) {
+    if (this.y + this.rad >= height || this.y - this.rad <= 0) {
+        console.log("edge");
         this.deltaY *= -1;
-    }
+      }
   }
 
   chibiCheck( otherChibis, myId) {
